39b17dcdec5f42d9a9e48339d94073bdf97fd8f4
Hisham Elsayed <hishamelsayed59@outlook.sa>
2025-09-05 14:36:12 +0300

    Initialize Realm globally in to  main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:e_commerce_app/core/routing/app_router.dart';
+import 'package:realm/realm.dart';
+import 'features/auth/models/user_model.dart';
+
+// Realm instance accessible globally
+late final Realm realm;
 
 void main() {
+  // Initialize Realm before runApp
+  final config = Configuration.local([UserModel.schema]);
+  realm = Realm(config);
+
   runApp(const ProviderScope(child: MyApp()));
 }
 
